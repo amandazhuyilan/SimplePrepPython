f491bc54d2c47d25c611cd9fe82ee713936368b4
Amanda <amanda.too@Amandas-MacBook-Air.local>
2024-11-15 10:39:26 +0900

    add parking system question

--- parking_system.py
+++ parking_system.py
@@ -1,51 +1,48 @@
-# Design a Parking Lot System
-# Problem: Implement a simple class structure for a parking lot system where you can park and remove cars. 
-# Include different types of vehicles and keep track of the available parking spaces.
-
 import unittest
 
-class ParkingLot:
+class ParkSystem:
     def __init__(self, capacity):
         self.capacity = capacity
         self.cars = []
+    
+    def park_car(self, car : str):
+        self.cars.append(car)
+        self.capacity -= 1
+        if self.capacity < 0:
+            raise Exception("No space left!")
+        return "Car parked!"
+    
+    def remove_car(self, car: str):
+        try:
+            self.cars.remove(car)
+        except Exception as e:
+            raise e
+        return "Car out of the parking lot!"
+    
 
-    def park_car(self, car_to_be_parked: str) -> bool:
-        """
-        park the damn car
-
-        args:
-            car_to_be_parked (str): the car to be parked
-        """
-        if len(self.cars) > self.capacity:
-            return "Parking lot full!"
-        else:
-            self.capacity -= 1
-            self.cars.append(car_to_be_parked)
-            return "Car parked!"
-
-    def remove_car(self, car_to_be_removed: str) -> bool:
-        if car_to_be_removed not in self.cars:
-            return "Car to be removed not available!"
-
-        self.cars.remove(car_to_be_removed)
-        self.capacity += 1
-        return "Car removed!"
-
-class TestParkingLot(unittest.TestCase):
+class TestParkSystem(unittest.TestCase):
     def setUp(self):
-        self.lot = ParkingLot(100)
-
+        self.parking_system = ParkSystem(1)
+    
     def test_park_car(self):
-        response = self.lot.park_car("BMW X3")
+        response = self.parking_system.park_car("BMW X3")
         self.assertEqual(response, "Car parked!")
 
     def test_remove_car(self):
-        response = self.lot.remove_car("BMW X3")
-        self.assertEqual(response, "Car removed!")
+        self.parking_system.park_car("BMW X3")
+        response = self.parking_system.remove_car("BMW X3")
+        self.assertEqual(response, "Car out of the parking lot!")
 
-    def test_remove_car_fail(self):
-        response = self.lot.remove_car("BMW X1")
-        self.assertEqual(response, "Car to be removed not available!")
+    def test_error_with_full_parking_lot(self):
+        self.parking_system.park_car("car 1")
+        with self.assertRaises(Exception):
+            self.parking_system.park_car("car 2")
+
+    def test_error_with_none_existing_car(self):
+        self.parking_system.park_car("car 3")
+        with self.assertRaises(Exception):
+            self.parking_system.remove_car("car 4")
+
 
 if __name__ == "__main__":
     unittest.main()
